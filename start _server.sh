9b7f5d0a3acf71d10cc64649ca677d4d67136a39
Abdulqoyyum Aileru <aileruenterprise@gmail.com>
2024-11-06 09:25:40 +0100

    Add README

--- start _server.sh
+++ start _server.sh
@@ -3,11 +3,7 @@
 # Activate virtual environment if you have one
 # source /path/to/venv/bin/activate
 
-# Start Django server in the background
+# Start Django server
 echo "Starting Django server..."
-python manage.py runserver 0.0.0.0:8000 &
-
-# Start Celery worker in the background
-echo "Starting Celery worker..."
-celery -A e_academy worker --loglevel=info
+python manage.py runserver 0.0.0.0:8000
 
